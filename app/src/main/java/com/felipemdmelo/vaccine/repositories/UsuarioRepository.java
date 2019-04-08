d6acf0cf08b0d77823383ff07e156bce306bd205
Felipe Melo <felipe.melo@intelectah.com.br>
2019-04-08 18:04:33 -0300

    Ajustando erro de compilacao

--- app/src/main/java/com/felipemdmelo/vaccine/repositories/UsuarioRepository.java
+++ app/src/main/java/com/felipemdmelo/vaccine/repositories/UsuarioRepository.java
@@ -2,9 +2,12 @@ package com.felipemdmelo.vaccine.repositories;
 
 import com.felipemdmelo.vaccine.models.Usuario;
 
+import java.sql.Date;
+
 public class UsuarioRepository {
 
     public Usuario validaLogin(String login, String senha) {
+        Date dataNascimento = Date.valueOf("1988-10-21");
         return new Usuario("123321123321", "Felipe Melo", dataNascimento);
     }
 
